891a275a7f53f77b68804a155a3b9b5a016336b8
shiro-inoue <shiro_inoue@sobal.co.jp>
2020-11-11 09:18:25 +0900

    JSONアクセスをfileIOへ移動

---
+++ fileIO.js
@@ -0,0 +1,18 @@
+function readJSON() {
+    const fileSelect = document.getElementById("readJSON");
+    if (fileSelect.files.length == 0) {
+        return;
+    }
+
+    let reader = new FileReader();
+    reader.readAsText(fileSelect.files[0]);
+    reader.onloadend = () => {
+        console.log(reader.result);
+        block_id = JSON.parse(reader.result);
+        createTable(block_id);
+    }
+}
+
+function writeJSON() {
+    alert("writeJSON() 未実装");
+}
